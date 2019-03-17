a53c80339bb398b4937ceb84f1a59e9b6a02feb4
Demjan Kaluzki <kaluzkidemjan@gmail.com>
2019-03-17 11:38:40 +0100

    simplify autoload files approach

--- src/autoload-files.php
+++ src/autoload-files.php
@@ -3,10 +3,4 @@
  * Copyright (C) php-fn. See LICENSE file for license details.
  */
 
-foreach ([
-    '\fn\cli'      => 'functions-cli.php',
-] as $fnc => $file) {
-    if (!function_exists($fnc)) {
-        require_once __DIR__ . "/fn/$file" ;
-    }
-}
+require_once __DIR__ . '/functions-cli.php';
